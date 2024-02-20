c1b39436abae4f574748379d2c1b1fcf23afc927
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2024-02-19 22:45:26 -0500

    move global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import "./globals.css"
+import "../globals.css"
 
 import "@mantine/core/styles.css"
 import { ColorSchemeScript, MantineProvider } from "@mantine/core"
