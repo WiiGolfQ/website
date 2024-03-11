cc6e0de260de1ac22586c25ff613aa67371e0de8
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2024-03-11 17:42:05 -0400

    change api url to env

--- src/lib/fetchApi.ts
+++ src/lib/fetchApi.ts
@@ -1,6 +1,6 @@
 export const fetchApi = async (route: string): Promise<any> => {
-    // const apiUrl = process.env.API_URL;
-    const apiUrl = "http://localhost:8000/api";
+    const apiUrl = process.env.API_URL;
+    // const apiUrl = "http://localhost:8000/api";
 
     const response = await fetch(apiUrl + route, {
         method: "GET",
