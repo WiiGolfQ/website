1d57f3fe39c53ffcb7ccbed7ab2a1c2812726dc8
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2024-03-11 18:17:57 -0400

    env variable issue

--- src/lib/fetchApi.ts
+++ src/lib/fetchApi.ts
@@ -1,6 +1,11 @@
 export const fetchApi = async (route: string): Promise<any> => {
-    const apiUrl = process.env.API_URL;
-    // const apiUrl = "http://localhost:8000/api";
+    // const apiUrl = process.env.API_URL;
+
+    // TODO: env variables are not wanting to work
+    // pretty sure its a nextjs issue, but it was fixed? idk
+    // https://github.com/vercel/next.js/pull/54203
+
+    const apiUrl = "http://backend/api";
 
     const response = await fetch(apiUrl + route, {
         method: "GET",
