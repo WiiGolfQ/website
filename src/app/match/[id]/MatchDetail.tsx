6e619801fd7292959f3767a013fbef118146c261
vncz14 <57498723+vncz14@users.noreply.github.com>
2024-05-25 02:37:18 -0400

    playersdetail

--- src/app/match/[id]/MatchDetail.tsx
+++ src/app/match/[id]/MatchDetail.tsx
@@ -4,13 +4,25 @@ import CustomCard from "@/components/CustomCard";
 import TeamSummaryRow from "@/components/homepage/TeamSummaryRow";
 
 const MatchDetail = ({ match }: any /* any is temp */) => {
+    const PlayersDetail = ({ players }: any) => (
+        <Box>
+            {players.map((player: any) => (
+                <Accordion
+                    key={player.player.discord_id}
+                    summary={player.player.username}
+                    details="yt video goes here"
+                />
+            ))}
+        </Box>
+    );
+
     return (
         <CustomCard>
             {match.teams.map((team: any) => (
                 <Box key={team.pk}>
                     <Accordion
                         summary={<TeamSummaryRow team={team} showTeamNums />}
-                        details={JSON.stringify(team.players)}
+                        details={<PlayersDetail players={team.players} />}
                     />
                 </Box>
             ))}
