9f9263f839b6b858223d8feb0d44973739c30eda
vncz14 <57498723+vncz14@users.noreply.github.com>
2024-08-29 12:20:18 -0400

    local timestamp

--- src/utils/shortenTimestamp.ts
+++ src/utils/shortenTimestamp.ts
@@ -1,4 +1,4 @@
 export default function shortenTimestamp(timestamp: string) {
-    const colonIndex = timestamp.lastIndexOf(":");
-    return timestamp.replace("T", " ").substring(0, colonIndex);
+    const dayjs = require("dayjs");
+    return dayjs(timestamp).format("YYYY-MM-DD HH:mm");
 }
