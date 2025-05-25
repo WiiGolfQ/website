12a659b559919e534fdb8e1dc915c3687b8c2a4b
Vincenzo <57498723+vncz14@users.noreply.github.com>
2025-05-25 02:38:11 -0400

    Update useFetch.ts

--- src/lib/useFetch.ts
+++ src/lib/useFetch.ts
@@ -27,7 +27,7 @@ export const useFetch = (route: string) => {
     // pretty sure its a nextjs issue, but it was fixed? idk
     // https://github.com/vercel/next.js/pull/54203
 
-    const apiUrl = "https://api.four.courses/v1";
+    const apiUrl = "https://api.914000.xyz/v1";
 
     const { data, error, isLoading } = useSWR(`${apiUrl}${route}`, fetcher);
 
